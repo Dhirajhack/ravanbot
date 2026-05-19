58d121aef5fbc55146cb09aad5eaf729260ebbc9
Dhirajhack <122712894+Dhirajhack@users.noreply.github.com>
2026-05-19 04:27:02 +0000

    dhiraj6

--- bot.py
+++ bot.py
@@ -182,7 +182,7 @@ async def broadcast(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
             for user in users:
                 try:
-                    await context.bot.send_message(
+                    await context.bot.copy_message(
                         chat_id=user,
                         text=message
                     )
